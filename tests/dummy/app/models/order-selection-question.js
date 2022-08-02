f0d3cde61caf89f4b6359c1b93b1f3705c34bd24
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-08-02 16:07:21 -0400

    update ember-cli

--- tests/dummy/app/models/order-selection-question.js
+++ tests/dummy/app/models/order-selection-question.js
@@ -7,15 +7,15 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 @buildValidations({
   order: {
     description: 'Order',
-    validators: [validator('ds-error'), validator('presence', true)],
+    validators: [validator('presence', true)],
   },
   selection: {
     description: 'Order Selection',
-    validators: [validator('ds-error'), validator('presence', true)],
+    validators: [validator('presence', true)],
   },
   text: {
     description: 'Question Text',
-    validators: [validator('ds-error'), validator('presence', true)],
+    validators: [validator('presence', true)],
   },
 })
 export default class OrderSelectionQuestion extends Model {
--- tests/dummy/app/models/order-selection.js
+++ tests/dummy/app/models/order-selection.js
@@ -8,7 +8,6 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
   quantity: {
     description: 'Quantity',
     validators: [
-      validator('ds-error'),
       validator('number', {
         gte: 1,
       }),
@@ -17,19 +16,17 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
   order: {
     description: 'Order',
     validators: [
-      validator('ds-error'),
       validator('belongs-to'),
       validator('presence', true),
     ],
   },
   line: {
     description: 'Order Line',
-    validators: [validator('ds-error'), validator('presence', true)],
+    validators: [validator('presence', true)],
   },
   questions: {
     description: 'Order Selection Questions',
     validators: [
-      validator('ds-error'),
       validator('has-many'),
       validator('length', {
         min: 1,
